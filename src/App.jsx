467c0671658d30499b9f6156097eed4b8091ff85
Andre Lucas <andrelucca99@gmail.com>
2023-11-29 13:22:43 -0300

    feat: add component Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { styled } from "styled-components";
 import EstilosGlobais from "./components/EstilosGlobais";
 import Header from "./components/Header";
 import BarraLateral from "./components/BarraLateral";
+import Banner from "./components/Bannner";
+import bgBanner from "./assets/banner.png";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154582 96.76%);
@@ -15,6 +17,7 @@ function App() {
       <EstilosGlobais />
       <Header />
       <BarraLateral />
+      <Banner texto={'A galeria mais completa de fotos do espaço!'} bgImage={bgBanner} />
     </FundoGradiente>
   )
 }
---
+++ src/components/Bannner/index.jsx
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+const FigureStyled = styled.figure`
+  background-image: ${props => `url(${props.$bgImage})`};
+    flex-grow: 1;
+    background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+    min-height: 328px;
+    margin: 0;
+    border-radius: 20px;
+    width: 1156px;
+    max-width: 100%;
+`;
+
+const TituloStyled = styled.h1`
+  max-width: 300px;
+  padding: 0 64px;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 48px;
+  color: #FFF;
+`;
+
+const Banner = ({ texto, bgImage }) => {
+  return (
+    <FigureStyled $bgImage={bgImage}>
+      <TituloStyled>{texto}</TituloStyled>
+    </FigureStyled>
+  )
+}
+
+export default Banner;
