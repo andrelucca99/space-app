09b2013f909074dff91bf3c4c2732a4645bfd9d7
Andre Lucas <andrelucca99@gmail.com>
2023-11-29 14:48:45 -0300

    style: add estilos nas tags

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,6 +1,49 @@
+import { styled } from "styled-components";
+import tags from "./tags.json";
+
+const TagsContainer = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 64px;
+  margin-top: 56px;
+`;
+
+const TagTitulo = styled.h3`
+  font-weight: 400;
+  font-size: 24px;
+  color: #D9D9D9;
+  margin: 0;
+`;
+
+const TagStyled = styled.div`
+  display: flex;
+  gap: 24px;
+  justify-content: end;
+`;
+
+const TagButton = styled.button`
+    font-size: 24px;
+    color: #FFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #C98CF1;
+    }
+`;
+
 const Tags = () => {
   return (
-    <p>Tags</p>
+    <TagsContainer>
+      <TagTitulo>Busque por tags:</TagTitulo>
+      <TagStyled>
+        {tags.map(tag => <TagButton key={tag.id}>{tag.titulo}</TagButton>)}
+      </TagStyled>
+    </TagsContainer>
   )
 }
 
