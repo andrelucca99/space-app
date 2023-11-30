6d9f8c7a885cc91dd04cdb1d9ba42b6de5d93f21
Andre Lucas <andrelucca99@gmail.com>
2023-11-30 10:04:09 -0300

    feat: implementa component Populares na galeria

--- src/components/Galeria/Populares/index.jsx
+++ src/components/Galeria/Populares/index.jsx
@@ -1,10 +1,40 @@
+import { styled } from "styled-components";
 import Titulo from "../../Titulo";
+
+import fotosPopulares from "./fotos-populares.json";
+
+const ColunaFotos = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Imagem = styled.img`
+  max-width: 212px;
+  border-radius: 20px;
+`;
+
+const Botao = styled.button`
+  background-color: transparent;
+  color: #fff;
+  border: 2px solid #C98CF1;
+  padding: 12px 20px;
+  font-size: 20px;
+  border-radius: 10px;
+  cursor: pointer;
+  width: 100%;
+  margin-top: 16px;
+`;
 
 const Populares = () => {
   return (
-    <div style={{ minWidth: 212 }}>
+    <section>
       <Titulo $alinhamento="center">Populares</Titulo>
-    </div>
+      <ColunaFotos>
+        {fotosPopulares.map(foto => <Imagem key={foto.id} src={foto.path} alt={foto.alt} />)}
+      </ColunaFotos>
+      <Botao>Ver mais</Botao>
+    </section>
   )
 }
 
